32cf5b7469cf6e54056ae0e259247e6298e19975
darkersu <sxp_developer@163.com>
2023-03-25 10:57:20 +0800

    feat: algolia 配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,10 +12,11 @@ export default defineConfig({
     themeConfig: {
         nav,
         sidebar,
-        algolia:{
-            appId:'TT98URW4E7',
-            apiKey:'95ddb6f2e6f911858408217b80c13d9b',
-            indexName:'iMarker'
+        algolia: {
+            appId: 'TT98URW4E7',
+            apiKey: '95ddb6f2e6f911858408217b80c13d9b',
+            indexName: 'iMarker',
+            placeholder: '请输入关键词',
         }
     }
 })
